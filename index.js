dd7569abdb97c4f4d96b443ae6ed8ccfe7cdf5bd
Keith Chia <keith.chiawj@axa.com.sg>
2018-07-13 11:22:26 +0800

    bobot replies to everything; add response keyboard

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Telegraf = require('telegraf')
 require('dotenv').config()
 const app = new Telegraf(process.env.BOT_TOKEN)
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
 
-app.hears('hello', ctx => {
-  return ctx.reply('GUAVA GUAVA!')
+app.hears(/./g, ctx => {
+  return ctx.replyWithMarkdown('`GUAVA GUAVA`', Extra.markup(Markup.keyboard(['Guava guava?'])))
 })
 
 app.startPolling()
