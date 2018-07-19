3be6232f770d7a9d3f9c4f3a3af983fc2a67d54d
Keith Chia <email@keithchia.com>
2018-07-19 23:24:02 +0800

    add pouchdb

---
+++ db.js
@@ -0,0 +1,4 @@
+const PouchDB = require('pouchdb')
+const db = new PouchDB('db')
+
+module.exports = db
--- index.js
+++ index.js
@@ -1,43 +1,79 @@
 const Telegraf = require('telegraf')
 const axios = require('axios')
+const moment = require('moment')
 require('dotenv').config()
 const app = new Telegraf(process.env.BOT_TOKEN)
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
-// const checkClasses = require('./puppeteer/checkClassMap')
 const checkClassUrl = require('./puppeteer/checkClassUrl')
-const linkRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+const db = require('./db')
 
+// eslint-disable-next-line
+const linkRegex = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
 const email = process.env.EMAIL
 const password = process.env.PASSWORD
 
-app.context.db = (() => {
-  let value = 0
-  return {
-    get: () => value,
-    add: val => {
-      value += val
-    }
-  }
-})()
+app.use((ctx, next) => {
+  // if (ctx.chat.username !== 'wekkit') return next()
+  const timestamp = moment().format('MMMM D, h:mm:ss a')
+  console.log(`[${timestamp}] ${ctx.chat.username}: ${ctx.message.text}`)
+  return next()
+})
+
+// app.hears('db', ctx => {})
+
+app.hears('hello', ctx => {
+  const response = `hello, ${ctx.chat.username}!`
+  return ctx.replyWithMarkdown(`\`${response}\``)
+})
 
 app.hears('yesno', ctx => {
   axios
     .get('https://yesno.wtf/api')
     .then(res => {
       const response = `bobot says ${res.data.answer}`
-      return ctx.replyWithMarkdown(`\`${response}\``)
+      return ctx
+        .replyWithVideo(res.data.image)
+        .then(() => ctx.replyWithMarkdown(`\`${response}\``))
     })
-    .catch(console.log)
+    .catch(console.error)
 })
 
 app.hears('get', ctx => {
-  return ctx.replyWithMarkdown(`\`${ctx.db.get()}\``)
+  db.get(ctx.chat.username).then(doc =>
+    ctx.replyWithMarkdown(`\`${doc._id}: ${doc.count}\``)
+  )
 })
 
 app.hears('add', ctx => {
-  ctx.db.add(1)
-  return ctx.replyWithMarkdown('`added 1`')
+  db.get(ctx.chat.username).then(doc => {
+    doc.count++
+    db.put(doc).then(() =>
+      ctx.replyWithMarkdown(`\`${doc._id}: ${doc.count}\``)
+    )
+  })
+})
+
+app.hears('put', ctx => {
+  const doc = {
+    _id: ctx.chat.username,
+    count: 0
+  }
+  db.put(doc)
+    .then(() => ctx.replyWithMarkdown('`put`'))
+    .catch(console.log)
+})
+
+app.hears('clear', ctx => {
+  db.allDocs({ include_docs: true })
+    .then(allDocs =>
+      allDocs.rows.map(row => ({
+        _id: row.id,
+        _rev: row.doc._rev,
+        _deleted: true
+      }))
+    )
+    .then(deleteDocs => db.bulkDocs(deleteDocs))
 })
 
 app.hears(linkRegex, async ctx => {
